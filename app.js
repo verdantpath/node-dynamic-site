0052756ad4ed542464e506e082d915cccbad96f9
verdantpath <m_dittmann@yahoo.com>
2018-12-31 12:26:36 -0800

    refactor

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var router = require("./router.js");
 
 var http = require('http');
 http.createServer(function (request, response) {
-
+  router.home(request, response);
+  router.user(request, response);
 }).listen(3000);
-console.log('Server is really running at http://<workspace-url>/');
+console.log('Server running at http://<workspace-url>/');
 
 
 // Function that handles the reading of files and merge in value
