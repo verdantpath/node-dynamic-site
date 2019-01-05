4ff1b9787a1603136e8b9287222d04bda24b8401
verdantpath <m_dittmann@yahoo.com>
2019-01-05 14:31:24 -0800

    get and post requests

--- router.js
+++ router.js
@@ -1,21 +1,34 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var querystring = require('querystring');
 
-var commonHeaders = {'Content-Type': 'text/plain'};
+var commonHeaders = {'Content-Type': 'text/html'};
 
 // Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   //if url == "/" && GET
   if(request.url == "/") {
-    //show search
-    response.writeHead(200, commonHeaders);
-    renderer.view("header", {}, response);
-    renderer.view('search', {}, response);
-    renderer.view('footer', {}, response);
-    response.end();
+    if (request.method.toLowerCase() === "get") {
+      //show search
+      response.writeHead(200, commonHeaders);
+      renderer.view("header", {}, response);
+      renderer.view('search', {}, response);
+      renderer.view('footer', {}, response);
+      response.end();
+    } else {
+
+    }
   }
   //if url == "/" && POST
-    //redirect to /:username
+    // get the post data from the body
+    request.on('data', function(postBody) {
+      // extract the username
+      var query = querystring.parse(postBody.toString());
+      response.write(query.username);
+        response.end();
+
+      //redirect to /:username
+    });
 }
 // Handle HTTP route GET /:username i.e. /chalkers
 function user(request, response) {
